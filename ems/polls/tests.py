6b837e00cac2dc49247037710f7cc29e281e1a8c
BTISAVB\arnau.villoro <villoro7@gmail.com>
2018-09-14 10:26:58 +0200

    [TST] add more tests for was_published_recently

--- ems/polls/tests.py
+++ ems/polls/tests.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import timedelta
 
 from django.utils import timezone
 from django.test import TestCase
@@ -13,6 +13,21 @@ class QuestionMethodTests(TestCase):
         was_published_recently() should return False for questions whose
         pub_date is in the future.
         """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        future_question = Question(pub_date=timezone.now() + timedelta(days=30))
         self.assertFalse(future_question.was_published_recently())
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        old_question = Question(pub_date=timezone.now() - timedelta(days=30))
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        recent_question = Question(pub_date=timezone.now() - timedelta(hours=1))
+        self.assertTrue(recent_question.was_published_recently())
